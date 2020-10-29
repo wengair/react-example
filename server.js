090b3101727f2a3ca40c0d31d51ee848e352cadd
Chia-Tse Weng <wengyagami@hotmail.com>
2020-10-29 14:45:56 -0700

    for testing heroku

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require('express')
+const express = require('express')
 const path = require("path");
-var app = express()
-// var mongoose = require('mongoose')
+const app = express()
+const cors = require('cors')
 
-var cors = require('cors')
+// const mongoose = require('mongoose')
 // mongoose connection
 // mongoose.connect('mongodb://localhost:27017/hellodb')
 
+// general settings
 app.use(cors())
-// CORS Middleware
 app.use((req, res, next) => {
   // Enabling CORS
   res.header('Access-Control-Allow-Origin', '*')
@@ -17,21 +17,18 @@ app.use((req, res, next) => {
   next()
 })
 
-app.get('/recipeApp', (req, res) => {
-  res.send('this is Recipe app building up soon')
-  console.log('initial test of the app')
-})
-// require('./services/scada.js')
-app.use(express.static('../build'));
-
-var endpoints = require('./services/endpoints.js') // file name doesn't affect
+// API
+const endpoints = require('./services/endpoints.js') // file name doesn't affect
 app.use('/api/v1', endpoints)
-// app.use(function(req, res) {
-// 	res.sendFile(path.join(__dirname, ' ../build/index.html'));
-// });
+
+// deployment
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, ' ../build'))
+});
 
 // Setting up server
-var Port = process.env.PORT || 8080
+const Port = process.env.PORT || 8080
 app.listen(Port, () => {
   console.log('App now running on port', Port)
 })
