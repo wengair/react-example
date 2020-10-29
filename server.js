035c5ef378616093d599442e77c3136c5767a76f
Chia-Tse Weng <wengyagami@hotmail.com>
2020-10-29 15:31:01 -0700

    change senfFile to use

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/v1', endpoints)
 
 // deployment
 app.use(express.static(path.join(__dirname, './frontend/build')))
-app.get('*', (req, res) => {
+app.use((req, res) => {
 	res.sendFile(path.join(__dirname, './frontend/build/index.html'))
 });
 
