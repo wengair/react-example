ac8fcbcbb841f431f49826eb67f294768f50a0e8
Chia-Tse Weng <wengyagami@hotmail.com>
2020-10-29 23:14:06 -0700

    modify config

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/v1', endpoints)
 
 // deployment
 app.use(express.static(path.join(__dirname, './frontend/build')))
-app.use((req, res) => {
+app.use('/', (req, res) => {
   res.sendFile(path.join(__dirname, './frontend/build/index.html'))
 })
 
