79f2461660e25b7423daaf68a08100563841da94
Chia-Tse Weng <wengyagami@hotmail.com>
2020-10-29 22:36:54 -0700

    delete proxy and add api consition

--- frontend/src/lib/config.js
+++ frontend/src/lib/config.js
@@ -1,6 +1,6 @@
 export const config = {
   sous: {
     domain: 'http://127.0.0.1:3000',
-    apiUrl: 'http://127.0.0.1:8080/api/v1/',
+    apiUrl: !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8080/api/v1/' : 'https://recipe-mern-test.herokuapp.com/api/v1/',
   },
 }
