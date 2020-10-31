c0f2b0fea9b4d09664800bcab2d945a0eb90dd87
Chia-Tse Weng <wengyagami@hotmail.com>
2020-10-31 09:42:47 -0700

    Updated the modification for Heroku deployment

--- services/endpoints.js
+++ services/endpoints.js
@@ -13,15 +13,6 @@ const recipes = require('../recipeRecord/findByIngredients')
 const recipeInfo = require('../recipeRecord/recipeInformationWithNutrition')
 const {endpointError, logError} = require('./util')
 
-app.use(function(req, res, next) {
-  // Enabling CORS
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization')
-  next()
-})
-
-
 router.route('/findByIngredients').all(jsonParser).post(async (req, res) => {
   // const queryString = req.body.queryIngredients.join(',+')
   // try {
