fe9b2134535add34ed0ff701a215ca0cbdaedf18
b-mp-e <brandon.mp.ellis@gmail.com>
2020-10-31 17:50:02 -0700

    Results now return 6 recipes per page. Cleaned up footer code.

--- frontend/src/views/SearchResult.js
+++ frontend/src/views/SearchResult.js
@@ -4,6 +4,7 @@ import {config} from '../lib/config'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import {Link} from 'react-router-dom'
 import LikeIcon from '../images/like.svg'
+import { set } from 'mongoose'
 
 const urlJoin = require('url-join')
 
@@ -11,7 +12,11 @@ function SearchResult({location}) {
   // when redirected form landing page, the user's query will be put in location.state.queryString
   const [queryString, setQueryString] = useState(location.state.queryString)
   const [refresh, setRefresh] = useState(false)
-  const [recipies, setRecipies] = useState()
+  const [recipes, setRecipes] = useState() 
+  const [resultPerPage, setResulPerPage] = useState(6) //The number of results to show on a "page"
+  const [pageNum, setPageNum] = useState(1) //The current page number, in terms of what group of results are shown
+  const [pageRecipes, setPageRecipes] = useState() //Array of recipes shown in quantity of resultPerPage 
+  
 
   const submitHandler = (e) => {
     e.preventDefault()
@@ -20,52 +25,95 @@ function SearchResult({location}) {
     setRefresh(true)
   }
 
-  const fetchRecipies = async () => {
+
+  const fetchRecipes = async () => {
     const queryIngredients = queryString.replace(', ', ',').split(',')
     console.log(queryIngredients, 'queryIngredients')
+    console.log(process.env.NODE_ENV,'process.env.NODE_ENV')
     fetch(urlJoin(config.sous.apiUrl, 'findByIngredients'), {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
+        'accept': 'application/json',
       },
       body: JSON.stringify({
         queryIngredients: queryIngredients,
       }),
     })
       .then(res => res.json())
-      .then(data => setRecipies(data.recipes))
+      .then(data => setRecipes(data.recipes))
+      .catch(e => console.log(e))
+  }
+
+  const getShowRecipes = () => {
+    if(recipes) {
+      setPageRecipes(recipes.slice((pageNum - 1) * resultPerPage, (pageNum - 1) * resultPerPage + resultPerPage))
+    }
   }
   
   useEffect(() => {
-    fetchRecipies()
+    fetchRecipes()
     return () => {
       setRefresh(false)
     }
   }, [refresh])
 
+  //After recipes or pageNum are changed, get the correct group of recipes to show
+  useEffect(() => {
+    getShowRecipes()
+  }, [recipes, pageNum])
+
+  //Change the current page 
+  const changePage = (nextPage) => {
+    //Out of scope check
+    //  First page to last page
+    if (nextPage == 0) {
+      setPageNum(Math.ceil(recipes.length / resultPerPage))
+    }
+    
+    //Out of scope check
+    //  Last page to first page
+    else if (nextPage == Math.ceil(recipes.length / resultPerPage) + 1) {
+      setPageNum(1)
+    }
+    
+    //Go to the page requested
+    else {
+      setPageNum(nextPage) 
+    }
+  }
+  
   return (
     <div>
       <div className='searchbar-container'>
         <SearchBar onSubmit={submitHandler} queryString={queryString} setQueryString={setQueryString} />
       </div>
-      {/* if recipes has value, print all recipies' title */}
-      {/* we need "recipies &&" to not show anything since recipies will recieve data later (in useEffect) */}
+      {/* if recipes has value, print all recipes' title */}
+      {/* we need "recipes &&" to not show anything since recipes will recieve data later (in useEffect) */}
       <div className='result-wrapper'>
-        {recipies && recipies.map(recipe => {  
+        {pageRecipes && pageRecipes.map(recipe => {
           return (
-            <div className='recipe-card'>
+            <div className='recipe-card' id="test_search_result_recipe_card">
               <Link to={`/recipe/${recipe.id}`}>
                 <div>
-                  <img src= {recipe.image} className='recipe-img' />
+                  <img src={recipe.image} className='recipe-img' id="test_search_result_recipe_image" alt={recipe.title} />
                   <div className='reicpe-text'>
-                    <p className='recipe-name'> {recipe.title} </p>
-                    <img src={LikeIcon} alt='like icon' className='like-icon'/>
+                    <p className='recipe-name' id="test_search_result_recipe_title"> {recipe.title}</p>
+                    <img src={LikeIcon} alt='like icon' className='like-icon' id="test_search_result_recipe_like"/>
                   </div>
                 </div>
               </Link>
             </div>)
         })}
       </div>
+
+      <div className='button-container'>
+        <div className='button-wrapper'>
+          <button className='nav-button' id="test_navbutton_prev"onClick={() => changePage(pageNum - 1)}>previous</button>
+          <button className='nav-button' id="test_navbutton_next"onClick={() => changePage(pageNum + 1)}>next</button>
+        </div>
+      </div>
+      
       <style jsx='true'>
       {`
       .searchbar-container {
@@ -78,9 +126,43 @@ function SearchResult({location}) {
       .result-wrapper {
         background: rgba(252, 209, 127, 0.26);
         display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        padding: 50px;
+        grid-template-columns: 
+                1fr 1fr 1fr;
+        padding: 50px 50px 25px 50px;
         min-height: calc(100vh - 400px);
+      }
+
+      .button-container{
+        background: rgba(252, 209, 127, 0.26);
+        padding-bottom: 15px;
+      }
+
+      .button-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      
+      .nav-button {
+        background-color: white;
+        border: 2px solid #e7e7e7;
+        border-radius: 20px;
+        border: none;
+        color: black;
+        height: 30px;
+        width: 80px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 0px 180px;
+        transition-duration: 0.4s;
+        cursor: pointer;
+      }
+
+      .nav-button:hover {
+        background-color: var(--c-brown);
+        color: white;
       }
     
       .container-fluid{
@@ -92,6 +174,7 @@ function SearchResult({location}) {
         padding: 10px;
         object-fit: cover;
       }
+
       .recipe-img {
         width: 100%;
         
