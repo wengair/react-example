4686b9c57b6e543221d3035ec1fb1c0267da2095
JT <wengyagami@hotmail.com>
2020-11-12 10:11:26 -0800

    test creating muliple tables

--- migrations/20201112164310-initial-tables.js
+++ migrations/20201112164310-initial-tables.js
@@ -16,27 +16,19 @@ exports.setup = function(options, seedLink) {
 
 exports.up = function(db) {
   console.log('Building tables...')
-  const engineCharsetCollate = 'ENGINE=InnoDB CHARACTER SET=utf8mb4 COLLATE=utf8mb4_unicode_ci'
-
-  const createUsers = () => db.runSql(`
-    CREATE TABLE users (
-      id                  INT NOT NULL AUTO_INCREMENT,
-      email               VARCHAR(255) NOT NULL,
-      password            VARCHAR(255) NOT NULL,
-      first_name          VARCHAR(255),
-      last_name           VARCHAR(255),
-      created             DATETIME NOT NULL,
-      modified            DATETIME,
-      PRIMARY KEY (id)
-    ) ${engineCharsetCollate}
-  `)
-
-  return  createUsers()
-  .then(console.log('Build users table successfully'))
+  db.createTable('users', {
+    id: { type: 'int', primaryKey: true, autoIncrement: true },
+    name: 'string'  // shorthand notation
+  });
+  db.createTable('pets', {
+    id: { type: 'int', primaryKey: true, autoIncrement: true },
+    name: 'string'  // shorthand notation
+  });
 };
 
 exports.down = function(db) {
-  return db.runSql(`DROP TABLE users`);
+  db.dropTable('pets');
+  db.dropTable('users');
 };
 
 exports._meta = {
