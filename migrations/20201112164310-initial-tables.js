001a0413722883f635d19539f7fc60bbf9718843
JT <wengyagami@hotmail.com>
2020-11-12 09:29:54 -0800

    modify initial table settings

--- migrations/20201112164310-initial-tables.js
+++ migrations/20201112164310-initial-tables.js
@@ -16,23 +16,19 @@ exports.setup = function(options, seedLink) {
 
 exports.up = function(db) {
   console.log('Building tables...')
-  const engineCharsetCollate = 'ENGINE=InnoDB CHARACTER SET=utf8mb4 COLLATE=utf8mb4_unicode_ci'
-
-  const createUsers = () => db.runSql(`
-    CREATE TABLE users (
-      id                  INT NOT NULL AUTO_INCREMENT,   -- a uuid
-      email               VARCHAR(255) NOT NULL,
-      password            VARCHAR(255) NOT NULL,
-      first_name          VARCHAR(255),
-      last_name           VARCHAR(255),
-      created             DATETIME NOT NULL,
-      modified            DATETIME,
-      PRIMARY KEY (id)
-    ) ${engineCharsetCollate}
-  `)
-
-  return  createUsers()
-  .then(console.log('Build users table successfully'))
+  db.createTable('users', {
+    id: { 
+      type: 'int',
+      primaryKey: true,
+      autoIncrement: true
+    },
+    email: 'string',
+    password: 'string',
+    first_name: 'string',
+    last_name: 'string',
+    Created: 'date',
+    Modified: 'date',
+  }, callback);
 };
 
 exports.down = function(db) {
